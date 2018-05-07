428ea1fad8fc03b2e363407e233959379f413e6d
a18frilo <a18frilo@student.his.se>
2018-05-07 20:15:19 +0200

    upload assignment 3

--- app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -1,7 +1,20 @@
 package com.example.brom.activitiesapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static android.R.id.meddage;
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
@@ -13,25 +26,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("brom","onCreate() called.");
 
-        // 1. Create a ListView as in previous assignment
-        // 2. Create a new activity named "MountainDetailsActivity
-        // 3. Create a new Layout file for the MountainDetailsActivity called
-        //    "activity_mountaindetails". MountainDetailsActivity must have MainActivity as its
-        //    ´parent activity.
-        // 4. The layout file created in step 3 must have a LinearLayout
-        // 5. The layout file created in step 3 must be able to display
-        //    * Mountain Name
-        //    * Mountain Location
-        //    * Mountain Height
-        // 6. When tapping on a list item: create an Intent that includes
-        //    * Mountain Name
-        //    * Mountain Location
-        //    * Mountain Height
-        // 7. Display the MountainDetailsActivity with the data from the Intent created in step
-        //    6
-        // 8. From the MountainDetailsActivity you should have an option to "go back" using an
-        //    left arro button. This is done by letting the MainActivity be the parent activity to
-        //    MountainDetailsActivity.
+        List<String> MountainList = new ArrayList<String>(Arrays.asList(mountainNames));
+
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.content_mountain_details,R.id.testForMountains, MountainList);
+
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+        myListView.setAdapter(adapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               Intent intent = new Intent(getApplicationContext(), MountainDetailsActivity.class);
+               intent.putExtra("MountainName", mountainNames[position]);
+               intent.putExtra("MountainHeight", Integer.toString(mountainHeights[position]));
+               intent.putExtra("MountainLocation", mountainLocations[position]);
+               startActivity(intent);
+           }
+        });
     }
 }
---
+++ app/src/main/java/com/example/brom/activitiesapp/MountainDetailsActivity.java
@@ -0,0 +1,33 @@
+package com.example.brom.activitiesapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MountainDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mountaindetails);
+
+        Intent intent = getIntent();
+        String messageName = intent.getStringExtra("MountainName");
+        String messageHeight = intent.getStringExtra("MountainHeight");
+        String messageLocation = intent.getStringExtra("MountainLocation");
+        //Toast.makeText(getApplicationContext(), messageName+" "+messageHeight+" "+messageLocation , Toast.LENGTH_LONG).show();
+
+        TextView textViewName = (TextView)findViewById(R.id.text_MountainName);
+        TextView textViewLocation = (TextView)findViewById(R.id.text_MountainLocation);
+        TextView textViewHeight = (TextView)findViewById(R.id.text_MountainHeight);
+        textViewName.setText(messageName);
+        textViewLocation.setText(messageLocation);
+        textViewHeight.setText(messageHeight);
+    }
+}
